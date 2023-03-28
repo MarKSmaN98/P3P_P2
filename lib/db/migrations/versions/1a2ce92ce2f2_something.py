3a581959dd79d2d123c0ce85b12743c1152d4e61
Mark Coats <alexco1234@gmail.com>
2023-03-28 10:53:45 -0500

    I love breaking stuff....

---
+++ lib/db/migrations/versions/1a2ce92ce2f2_something.py
@@ -0,0 +1,24 @@
+"""something
+
+Revision ID: 1a2ce92ce2f2
+Revises: 
+Create Date: 2023-03-28 10:49:20.464167
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1a2ce92ce2f2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- lib/db/models.py
+++ lib/db/models.py
@@ -4,25 +4,25 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-# class Town(Base):
-#     __tablename__ = 'towns'
+class Town(Base):
+    __tablename__ = 'towns'
 
-#     id = Column(Integer(), primary_key=True)
-#     name = Column(String())
-#     state = Column(String())
-#     restaurants = relationship("Restaurant", backref=backref('restaurant'))
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    state = Column(String())
+    restaurants = relationship("Restaurant", backref=backref('restaurant'))
 
-#     def __repr__(self):
-#         return f"Name: {self.name}, State: {self.state}"
+    def __repr__(self):
+        return f"Name: {self.name}, State: {self.state}"
 
-# class Restaurant(Base):
-#     __tablename__ = 'restaurants'
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
 
-#     id = Column(Integer(), primary_key=True)
-#     name = Column(String())
-#     address = Column(String())
-#     phone = Column(Integer())
-#     town_id = Column(Integer(), ForeignKey('towns.id'))
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    address = Column(String())
+    phone = Column(Integer())
+    town_id = Column(Integer(), ForeignKey('towns.id'))
 
-#     def __repr__(self):
-#         return f"Restaurant Name: {self.name}, Address: {self.address}, Phone: {self.phone}"
+    def __repr__(self):
+        return f"Restaurant Name: {self.name}, Address: {self.address}, Phone: {self.phone}"
