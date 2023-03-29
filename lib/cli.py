70fd808bc079246ff3107205ad614159f05e80a3
Raymond An <raymond_an@yahoo.com>
2023-03-29 10:31:14 -0400

    add first part of show_data

--- lib/cli.py
+++ lib/cli.py
@@ -1,4 +1,4 @@
-from db.models import *
+from db.models import Town, Restaurant, Review
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import time, random, os
@@ -7,9 +7,9 @@ import time, random, os
 
 class CLI:
     def __init__(self, user_input = "undefined"):
-        self.town = [town for town in session.query(Town)]
-        self.restaurant = [restaurant for restaurant in session.query(Restaurant)]
-        self.review = [review for review in session.query(Review)]
+        self.towns = [town for town in session.query(Town)]
+        self.restaurants = [restaurant for restaurant in session.query(Restaurant)]
+        self.reviews = [review for review in session.query(Review)]
         self.name = user_input
         self.start()
 
@@ -37,52 +37,31 @@ class CLI:
         self.start()
 
     def menu(self):
-        print("1) Retrieve Data")
+        print("1) Retrieve Data of Towns, Restaurants, Review")
         print("2) ...")
         print("3) ...")
         print("4) ...")
         print("5) Quit")
 
-    def get_data(self):
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("Data Catalogue")
-        for _ in range(5):
-            print('\n')
-        #sub menu
-        print("1) Towns")
-        print('2) Restaurants')
-        print('3) Reviews')
-        sel = input("Select a Catalogue")
-
-        #clear console again
-        #count = 0
-        #for item in list:
-        #   if count % 50 == 0:
-        #       input("Press Enter To Continue")
-        #   print(item)
-        #   count ++
-
-
-
     def start(self):
-        print('Welcome to ')
+        print('Welcome to PlatePal {self.name}')
         exit = False
         while exit == False:
             self.menu()
-            sel = input("What would you like to do?")
+            sel = input("What would you like to do?: ")
 
-            if sel == 1:
-                self.get_data(self)
-            elif sel == 2:
+            if sel == '1':
+                get_data(self)
+            elif sel == '2':
                 pass
 
-            elif sel == 3:
+            elif sel == '3':
                 pass
 
-            elif sel == 4:
+            elif sel == '4':
                 pass
 
-            elif sel == 5:
+            elif sel == '5':
                 exit = True
             
             else:
@@ -98,10 +77,43 @@ class CLI:
             # if z == 'q' or z == 'Q':
             #     exit = True
 
+def get_data(self):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Data Catalogue")
+    for _ in range(5):
+        print('  ')
+    #sub menu
+    print("1) Towns")
+    print('2) Restaurants')
+    print('3) Reviews')
+    sel = input("Select a Catalogue: ")
+    print('  ')
+    if sel == '1':
+        print_towns(self.towns)
+
+def print_towns(towns):
+    print('  ')
+    print('**Towns**')
+    print('  ')
+    for index, town in enumerate(towns):
+        print(f'{index+1}. {town.name}')
+
+    print('  ')
+
+    #clear console again
+    #count = 0
+    #for item in list:
+    #   if count % 50 == 0:
+    #       input("Press Enter To Continue")
+    #   print(item)
+    #   count ++
+
+
 
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///thriddb.db')
+    engine = create_engine('sqlite:///db/thirddb.db')
     Session = sessionmaker(bind=engine)
     session = Session()
-    CLI()
+    user_input = input("Enter Username: ")
+    CLI(user_input)
     time.sleep(5)
